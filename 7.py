ae3da788fe04a9dca3c6104e562de9dd62e979c5
resyst <resyst.now@gmail.com>
2025-08-19 19:52:02 -0500

    oops didn't use isqrt

--- 7.py
+++ 7.py
@@ -1,10 +1,10 @@
 from itertools import count, islice
-from math import sqrt
+from math import isqrt
 
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(sqrt(n)) + 1):
+    for i in range(2, isqrt(n) + 1):
         if n % i == 0:
             return False
     return True
